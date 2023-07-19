21ceaa464bab3aa3fed84ae445b43104faaf3310
BretzelEnjoyer <soumyankarm@gmail.com>
2023-07-19 16:30:27 +0200

    initial beta heroku deploy

--- server/server.js
+++ server/server.js
@@ -5,24 +5,47 @@ const cors = require('cors');
 require('dotenv').config();
 
 /* ENDPOINTS */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
+const HOST = '127.0.0.1';
 const surveyResponseAddress = process.env.SURVEY_RESPONSE_HANDLER_ENDPOINT;
 const surveyFinalResponseAddress = process.env.SURVEY_FINAL_RESPONSE_HANDLER_ENDPOINT;
 const analysisDataAddress = process.env.ANALYSIS_ENDPOINT;
 const testAddress = process.env.TEST_ENDPOINT;
 /* ENDPOINTS */
 
+/* MONGODB ENDPOINTS */
+const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI_USERNAME = process.env.MONGODB_URI_USERNAME;
+const MONGODB_URI_CLUSTER_NAME = process.env.MONGODB_URI_CLUSTER_NAME;
+const MONGODB_URI_PASSWORD = process.env.MONGODB_URI_PASSWORD;
+// Replace the placeholders in the MongoDB URI with the actual values
+const formattedURI = MONGODB_URI
+  .replace('<username>', MONGODB_URI_USERNAME)
+  .replace('<password>', MONGODB_URI_PASSWORD)
+  .replace('<cluster_name>', MONGODB_URI_CLUSTER_NAME);
+/* MONGODB ENDPOINTS */
+
 /* EXPRESS APPLICATION */
 const app = express();
 /* EXPRESS APPLICATION */
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/survey-system', {
+mongoose.connect(formattedURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => {
+  .then(async () => {
     console.log('Connected to MongoDB');
+    console.log("connection uri=",formattedURI);
+        // Ping the deployment
+        const connection = mongoose.connection;
+        const pingResult = await connection.db.admin().ping();
+    
+        if (pingResult.ok === 1) {
+          console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        } else {
+          console.log("Failed to ping your deployment. Check your connection settings.");
+        }
   })
   .catch((error) => {
     console.error('MongoDB connection error:', error);
