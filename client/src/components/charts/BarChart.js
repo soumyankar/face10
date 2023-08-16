222d305dabe23eaf87fc944f69d6ca0690b6ecbc
BretzelEnjoyer <soumyankarm@gmail.com>
2023-08-16 17:18:33 +0530

    other dependency fixes

--- client/src/components/charts/BarChart.js
+++ client/src/components/charts/BarChart.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import Chart from 'chartjs/auto';
+import Chart from 'chart.js/auto';
 
 const BarChartComponent = ({ data, options }) => {
   const chartRef = useRef(null);
