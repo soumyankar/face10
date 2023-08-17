e1eb4ef728bf2725d0115cd65c06d44eaf66a182
BretzelEnjoyer <soumyankarm@gmail.com>
2023-08-17 09:29:51 +0530

    changed api routes

--- server/routes/surveyFinalHandler.mjs
+++ server/routes/surveyFinalHandler.mjs
@@ -6,15 +6,24 @@ const router = express.Router();
 
 // This section will help you create a new record.
 router.post("/", async (req, res) => {
-    const surveyResponseRawData = req.body; // Assuming the JSON data contains all the survey response fields
+  const surveyResponseRawData = req.body; // Assuming the JSON data contains all the survey response fields
+  
+  const surveyResponse = {
+    ...surveyResponseRawData,
+  };
+  
+  let collection = await db.collection("surveyFinal");
+  let result = await collection.insertOne(surveyResponse);
+  
+  // Check if insertion was successful and log the result
+  if (result.insertedCount === 1) {
+    console.log("Data insertion successful:", result.insertedId);
+  } else {
+    console.log("Data insertion failed.");
+  }
+  
+  
+  res.send(result).status(204);
+});
 
-    const surveyResponse = {
-      ...surveyResponseRawData,
-    };
-  
-    let collection = await db.collection("surveyFinal");
-    let result = await collection.insertOne(surveyResponse);
-    res.send(result).status(204);
-  });
-  
-  export default router;
+export default router;
